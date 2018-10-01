f510396ead677fe606004a5272e71a86ec9ac2f1
Jacob Moore <jmooree30@gmail.com>
2018-10-01 03:51:27 -0400

    Switched variables to ES6 syntax and removed old comments

--- playground/promise.js
+++ playground/promise.js
@@ -1,31 +1,21 @@
-var asyncAdd = (a,b) => {
+let asyncAdd = (a, b) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(typeof a === 'number' && typeof b === 'number') {
+      if (typeof a === "number" && typeof b === "number") {
         resolve(a + b);
       } else {
-        reject('Arguements must be numbers');
+        reject("Arguements must be numbers");
       }
-    },1500);
+    }, 1500);
   });
 };
 
-// var somePromise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     //resolve('Hey it worked!');
-//     reject('Unable to fulfill promise');
-//   }, 2500);
-// });
-
-// somePromise.then((message) => {
-//   console.log('Success:', message);
-// }, (errorMessage) => {
-//   console.log('Error:', errorMessage);
-// });
-
-asyncAdd(4,'4').then((res) => {
-  console.log('Result:', res);
-  return asyncAdd(res, 33);
-}).then((res) => {
-  console.log(res);
-}).catch((error) => console.log(error));
+asyncAdd(4, "4")
+  .then(res => {
+    console.log("Result:", res);
+    return asyncAdd(res, 33);
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => console.log(error));
